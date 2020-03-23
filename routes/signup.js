792277417cf0c19e8d9e09a096380bfda9625074
james <jjbe.93@gmail.com>
2020-03-24 00:22:55 +0200

    added input validation

--- routes/signup.js
+++ routes/signup.js
@@ -9,9 +9,21 @@ router.get("/", function(req, res) {
 
 router.post("/", async (req, res) => {
   const { email, password, confirmPassword } = req.body;
+  let errors = [];
   try {
-    const hashesPassword = await bcrypt.hash(req.body.password, 10);
-    res.render("layout", { page: "login", title: "User Login" });
+    if (!password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)) {
+      errors.push(
+        "password must contain 8 characters and atleast 1 number, 1 uppercase and lowercase letter"
+      );
+      res.render("layout", {
+        page: "signup",
+        title: "SignUp",
+        errors: errors
+      });
+    } else {
+      const hashesPassword = await bcrypt.hash(req.body.password, 10);
+      res.redirect("/login");
+    }
   } catch (error) {
     res.render("layout", { page: "signup", title: "SignUp" });
   }
