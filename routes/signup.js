b8a8eddbffbf0b1b5b85bcb7b18c44fe135824d1
james <jjbe.93@gmail.com>
2020-03-24 02:54:13 +0200

    added functionality insert user on the DB

--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
 /* GET home page. */
 router.get("/", function(req, res) {
@@ -8,20 +9,46 @@ router.get("/", function(req, res) {
 });
 
 router.post("/", async (req, res) => {
-  const { email, password, confirmPassword } = req.body;
+  const { name, email, password, confirm_password } = req.body;
   let errors = [];
   try {
+    // Check password length and validity
     if (!password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)) {
       errors.push(
         "password must contain 8 characters and atleast 1 number, 1 uppercase and lowercase letter"
       );
+    }
+
+    // Check if given password is the same as the confirmpassword
+    if (password !== confirm_password) {
+      errors.push("passwords do not match");
+    }
+
+    // Must stay on current page if errors exist
+    if (errors.length > 0) {
       res.render("layout", {
         page: "signup",
         title: "SignUp",
-        errors: errors
+        errors: errors,
+        name,
+        email,
+        password,
+        confirm_password
       });
     } else {
-      const hashesPassword = await bcrypt.hash(req.body.password, 10);
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const newUser = new User({
+        name: name,
+        email: email,
+        password: hashedPassword
+      });
+      newUser.save((err, user) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(user);
+        }
+      });
       res.redirect("/login");
     }
   } catch (error) {
